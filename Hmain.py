2801c830366fcba4ed588cf13a7fc7ce013bf681
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-02 18:49:41 +0300

    Add string challenge

---
+++ Hmain.py
@@ -0,0 +1,20 @@
+name_input = input("Enter your name: ")
+upper_name = name_input.upper()
+name_length = len(name_input)
+print("your name in Capital: " + upper_name)
+print("The number of letters in your name is: " + str(name_length))
+
+
+
+  
+
+
+ 
+
+ 
+            
+
+
+
+
+
