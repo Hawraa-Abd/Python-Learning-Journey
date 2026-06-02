488fa8a07c0032cfecd001c79738deac5b82df06
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-02 19:40:14 +0300

    Update Hmain.py

--- Hmain.py
+++ Hmain.py
@@ -1,8 +1,18 @@
 grade = int(input("Enter your grade (0-100): "))
-if grade >= 50:
-    print("Congratulations! you passed. 🎉")
+if grade >= 90:
+    print("Excellent! Grade: A 🌟")
+elif grade >= 80:
+   print("Very Good! Grade: B 🎉")
+elif grade >= 70:
+    print("Good! Grade: C 👍")
+elif grade >= 50:
+    print("passed! Grade: D 🙂")
 else:
-    print("Sorry, you failed. keep trying! 💪")
+    print("Faild. keep trying! 💪")
+    
+
+
+
     
 
 
