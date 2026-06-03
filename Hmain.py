edd7ce6be9ad3b09bea5f31ee2df72c0ea92f537
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-04 01:41:38 +0300

    Fix price checker indentation and logic

--- Hmain.py
+++ Hmain.py
@@ -1,7 +1,9 @@
-student_grades = [85, 42, 90, 65, 49, 73]
-print("(☞ﾟヮﾟ)☞ Checking Student Results ☜(ﾟヮﾟ☜)")
-for grade in student_grades:
- if grade  >= 50:
-  print(f"Grade {grade}: Passed! 🎉")
+price_checket = [4 , 12 , 8 , 25 ]
+print("➡➡ price checket ⬅⬅ ")
+for price in price_checket:
+ if price >= 15:
+   print(f"price {price}: Expensive! 🌚")
  else:
-  print(f"Grade {grade}: Failed! 💔")
+   print(f"price {price}: Cheap! 😀")
+
+
