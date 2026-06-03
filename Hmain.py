1a367d9a9ddacbc73393f844ddcfc4c126b10abb
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-03 15:11:04 +0300

    Add grades Loop with conditional logic

--- Hmain.py
+++ Hmain.py
@@ -1,5 +1,7 @@
-courses = ["python" , "AI", "Data Science", "Cybersecurity"]
-print("--- My Course List ---")
-for x in courses:
-    print("Course name: " + x)
-
+student_grades = [85, 42, 90, 65, 49, 73]
+print("(☞ﾟヮﾟ)☞ Checking Student Results ☜(ﾟヮﾟ☜)")
+for grade in student_grades:
+ if grade  >= 50:
+  print(f"Grade {grade}: Passed! 🎉")
+ else:
+  print(f"Grade {grade}: Failed! 💔")
