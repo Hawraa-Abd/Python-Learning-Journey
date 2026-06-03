a4909e930d91f76c6dea5bd91d7ea444a01451a4
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-03 14:11:24 +0300

    Add Python lists training  code

--- Hmain.py
+++ Hmain.py
@@ -1,33 +1,5 @@
-grade = int(input("Enter your grade (0-100): "))
-if grade >= 90:
-    print("Excellent! Grade: A 🌟")
-elif grade >= 80:
-   print("Very Good! Grade: B 🎉")
-elif grade >= 70:
-    print("Good! Grade: C 👍")
-elif grade >= 50:
-    print("passed! Grade: D 🙂")
-else:
-    print("Faild. keep trying! 💪")
-    
-
-
-
-    
-
-
-
-
-
-  
-
-
- 
-
- 
-            
-
-
-
-
+courses = ["Python" , "AI" , "Data Science" , "Cybersecurity"]
+print("My corses:" , courses)
+print("First course is:", courses[0])
+print("Updated courses:" , courses)
 
