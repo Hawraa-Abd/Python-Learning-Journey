ca9ae9025790c2988688b0987357d6e629d778bf
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-07 00:35:19 +0300

    Add trip age checker challenge code

--- Hmain.py
+++ Hmain.py
@@ -1,9 +1,7 @@
-price_checket = [4 , 12 , 8 , 25 ]
-print("➡➡ price checket ⬅⬅ ")
-for price in price_checket:
- if price >= 15:
-   print(f"price {price}: Expensive! 🌚")
- else:
-   print(f"price {price}: Cheap! 😀")
-
-
+ages = [12 , 35, 17, 22, 14]
+print("--- Trip Age Checker --- ")
+for a in ages:
+ if a >= 18: 
+    print(f"Age {a}: Allowed to enter! ✅") 
+else:
+    print(f"Age {a}: Underage - Needs parents! 👪")
