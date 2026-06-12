ab0ace7955ed15a1ce3ec71df62a7618f30663fb
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-12 23:18:05 +0300

    Mastering Python return statement

--- Hmain.py
+++ Hmain.py
@@ -1,12 +1,4 @@
-def check_trip_ages(any_ages_list):
-    print("---Trip Eligibility Results ---")
-    for a in any_ages_list:
-        if a >= 18:
-            print(f"Age {a}: Allowed to enter! ✅")
-        else:
-            print(f"Age {a}: Underage - Needs Parents! 👪")
-group_A = [20, 15, 35, 12]
-group_B = [40, 17, 22]
-check_trip_ages(group_A)
-print("--------------------------")
-check_trip_ages(group_B)
+def multiply_numbers(x,y):
+    return (x*y)
+result_1 = multiply_numbers(4,5)
+print(f"My encoded multiplication is: {result_1}")
