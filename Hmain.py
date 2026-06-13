1f7dc01ecd54f19f02035f50383d43ba879e91d5
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-13 23:07:55 +0300

    Exploring Python Dictionaries: creation, update, and deletion

--- Hmain.py
+++ Hmain.py
@@ -1,4 +1,15 @@
-def make_double(number):
-    return number * 2
-result = make_double(10)
-print(f"The double value is: {result}")
+user_profile = {
+    "name": "Hawraa",
+    "role": "programmer",
+    "level": 3
+}
+print(user_profile)
+print(user_profile["name"])
+print(user_profile["role"])
+user_profile["level"] = 4
+user_profile["city"] = "Baghdad"
+print("After update:")
+print(user_profile)
+del user_profile["city"]
+print("After deleting city:")
+print(user_profile)
