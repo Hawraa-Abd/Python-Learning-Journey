acc2e7a3771d2c71a296f1ccbdd663b6bda12c4e
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-14 22:53:03 +0300

    Secure message shielding for staff🔰

--- Hmain.py
+++ Hmain.py
@@ -1,13 +1,13 @@
-staff_list = [
-    {"name": "Hawraa", "role": "Developer", "clearance_level": 5},
-    {"name": "Zahra", "role": "Assistant", "clearance_level": 2},
-    {"name": "Adel", "role": "Manage", "clearance_level": 4}
+project_list = [
+    {"sender": "Hawraa", "text": "top secret data", "is_encrypted": True},
+    {"sender": "Zahra", "text": "system firewalls are up", "is_encrypted": True},
+    {"sender": "Fatima", "text": "database backup complete", "is_encrypted": False},
 ]
-print("--- 🔰 Staring Security Clearance Check 🔰 ---")
-for employee in staff_list:
-    if employee["clearance_level"] >= 4:
-        secure_name = employee["name"]. upper()
-        print(f"🟢 ACCESS GRANTED: {secure_name} ({employee['role']}) - Code: [SECURE-10x]")
-    else:
-        print(f"🔴 ACCESS DENIED: {employee['name']} ({employee['role']}) - Low Level!")
-        print("------------------------------------------")
+print("-------🔐 Secret Message Shield 🔐--------")
+for message in project_list:
+    if message["is_encrypted"] == True:
+        print(f"🟢 [SAFE] Message from {message['sender']} is fully secured!")
+else:
+    secure_text = message["text"]. upper()
+    print(f"🔴 [RAW] {message['sender']} -> {secure_text}")
+    print("------------------------------------------")
