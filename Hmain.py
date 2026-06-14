47759768d7b38133d2ff4c9d66cbec25c3f7956a
Hawraa-Abd <allwlwhalsghyrh@gmail.com>
2026-06-14 16:13:04 +0300

    Staff clearance system using python

--- Hmain.py
+++ Hmain.py
@@ -1,15 +1,13 @@
-user_profile = {
-    "name": "Hawraa",
-    "role": "programmer",
-    "level": 3
-}
-print(user_profile)
-print(user_profile["name"])
-print(user_profile["role"])
-user_profile["level"] = 4
-user_profile["city"] = "Baghdad"
-print("After update:")
-print(user_profile)
-del user_profile["city"]
-print("After deleting city:")
-print(user_profile)
+staff_list = [
+    {"name": "Hawraa", "role": "Developer", "clearance_level": 5},
+    {"name": "Zahra", "role": "Assistant", "clearance_level": 2},
+    {"name": "Adel", "role": "Manage", "clearance_level": 4}
+]
+print("--- 🔰 Staring Security Clearance Check 🔰 ---")
+for employee in staff_list:
+    if employee["clearance_level"] >= 4:
+        secure_name = employee["name"]. upper()
+        print(f"🟢 ACCESS GRANTED: {secure_name} ({employee['role']}) - Code: [SECURE-10x]")
+    else:
+        print(f"🔴 ACCESS DENIED: {employee['name']} ({employee['role']}) - Low Level!")
+        print("------------------------------------------")
